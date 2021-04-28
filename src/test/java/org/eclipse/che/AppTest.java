5a88155efa03313f901bd948e4bda3adbdcee3a7
Sergii Kabashniuk <skabashniuk@redhat.com>
2021-04-28 14:51:47 +0300

    Basic tests

--- src/test/java/org/eclipse/che/AppTest.java
+++ src/test/java/org/eclipse/che/AppTest.java
@@ -1,20 +1,38 @@
 package org.eclipse.che;
 
+import io.fabric8.kubernetes.client.server.mock.KubernetesServer;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+/** Unit test for simple App. */
+public class AppTest {
+  private KubernetesServer serverMock;
+  @BeforeClass
+  public void setUpLogger() throws Exception {
+    SLF4JBridgeHandler.removeHandlersForRootLogger();
+    SLF4JBridgeHandler.install();
+  }
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        Assert.assertTrue(true);
-    }
+  @BeforeMethod
+  public void setUp() throws Exception {
+
+    serverMock = new KubernetesServer(true, true);
+    serverMock.before();
+  }
+
+
+  @AfterMethod
+  public void tearDown() {
+
+    serverMock.after();
+  }
+
+  @Test
+  public void shouldAnswerWithTrue() {
+    Assert.assertTrue(true);
+  }
 }
