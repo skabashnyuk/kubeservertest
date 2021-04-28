417df9aa1dca3c4c7eac518a0faa1897e9697d7f
Sergii Kabashniuk <skabashniuk@redhat.com>
2021-04-28 15:19:52 +0300

    Added clear error

--- src/test/java/org/eclipse/che/AppTest.java
+++ src/test/java/org/eclipse/che/AppTest.java
@@ -1,14 +1,9 @@
 package org.eclipse.che;
 
-import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodBuilder;
 import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClientException;
-import io.fabric8.kubernetes.client.Watch;
-import io.fabric8.kubernetes.client.Watcher;
 import io.fabric8.kubernetes.client.server.mock.KubernetesServer;
-import junit.framework.AssertionFailedError;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -16,12 +11,10 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
 /** Unit test for simple App. */
 public class AppTest {
   private KubernetesServer serverMock;
+
   @BeforeClass
   public void setUpLogger() throws Exception {
     SLF4JBridgeHandler.removeHandlersForRootLogger();
@@ -35,7 +28,6 @@ public class AppTest {
     serverMock.before();
   }
 
-
   @AfterMethod
   public void tearDown() {
 
@@ -44,15 +36,22 @@ public class AppTest {
 
   @Test
   public void testInCrudMode() throws InterruptedException {
-    KubernetesClient client = serverMock.getClient();
+    try {
+      KubernetesClient client = serverMock.getClient();
 
-    //CREATE
-    client.pods().inNamespace("ns1").create(new PodBuilder().withNewMetadata().withName("pod1").endMetadata().build());
+      // CREATE
+      client
+          .pods()
+          .inNamespace("ns1")
+          .create(new PodBuilder().withNewMetadata().withName("pod1").endMetadata().build());
 
-    //READ
-    PodList podList = client.pods().inNamespace("ns1").list();
-    Assert.assertNotNull(podList);
-    Assert.assertEquals(1, podList.getItems().size());
-
+      // READ
+      PodList podList = client.pods().inNamespace("ns1").list();
+      Assert.assertNotNull(podList);
+      Assert.assertEquals(1, podList.getItems().size());
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw e;
+    }
   }
 }
